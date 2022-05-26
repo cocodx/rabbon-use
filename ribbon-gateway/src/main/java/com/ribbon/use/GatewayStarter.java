a8c7ddb518ae4e851fd214a1c7f4ad5bd6c5b8ff
刘刚 <2cevening@gmail.com>
2022-05-27 06:57:39 +0800

    fixed-insert(ribbon-gateway):添加gateway网关

---
+++ ribbon-gateway/src/main/java/com/ribbon/use/GatewayStarter.java
@@ -0,0 +1,13 @@
+package com.ribbon.use;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayStarter.class,args);
+    }
+}
