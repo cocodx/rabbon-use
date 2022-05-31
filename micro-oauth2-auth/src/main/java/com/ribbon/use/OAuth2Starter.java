429b8c7620deca10ca2312c603bec29906bdb3e2
刘刚 <2cevening@gmail.com>
2022-06-01 06:51:50 +0800

    添加micro-oauth2-auth 模块

---
+++ micro-oauth2-auth/src/main/java/com/ribbon/use/OAuth2Starter.java
@@ -0,0 +1,12 @@
+package com.ribbon.use;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2Starter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2Starter.class,args);
+    }
+}
---
+++ micro-oauth2-auth/src/main/java/com/ribbon/use/constant/RedisConstant.java
@@ -0,0 +1,6 @@
+package com.ribbon.use.constant;
+
+public class RedisConstant {
+
+    public static final String RESOURCE_ROLES_MAP = "AUTH:RESOURCE_ROLES_MAP";
+}
